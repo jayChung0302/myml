43063b0d64593a812abcc95e631ea8d0e81244b3
jayChung0302 <whtnek@gmail.com>
2021-05-28 04:10:08 +0900

    TCR wip

--- deep-scratch/rope-tcr.py
+++ deep-scratch/rope-tcr.py
@@ -14,13 +14,15 @@ def to_rope(string):
 class Rope:
     def substring(self, start, length):
         return Substring(self, start, length)
-
 class String(Rope):
     def __init__(self, string):
         self.string = string
     
     def  __str__(self):
         return self.string
+    
+    def concatenate(self, other):
+        return self.string + other.string
         
 class Substring(Rope):
     def __init__(self, rope, start, length):
@@ -35,3 +37,4 @@ class Substring(Rope):
 assert str(to_rope("ABC")) == "ABC"
 assert str(to_rope("ABCDE").substring(1, 3)) == "BCD"
 assert str(to_rope("ABCDE").substring(1, 3).substring(1,1)) == "C"
+assert str(to_rope("ABC").concatenate(to_rope("DE"))) == "ABCDE"
