a44cdef944bc0dc64db25efc43314a6d0829c157
jayChung0302 <whtnek@gmail.com>
2021-05-28 01:23:37 +0900

    TCR wip

--- deep-scratch/rope-tcr.py
+++ deep-scratch/rope-tcr.py
@@ -1,7 +1,14 @@
-# Rope
+# Rope data structure
+# add very large string, how can you insert single character into the middle of it in constant time?
 
 # todo
 # insert
 # delete
 # substring
 # concatenation
+
+# API
+def to_rope(string):
+    return string
+
+assert str(to_rope("ABC")) == "ABC"
