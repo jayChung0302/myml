386958ed8535344f198ddee3f8522a94981cd188
jayChung0302 <whtnek@gmail.com>
2021-05-29 13:21:26 +0900

    wip step21

---
+++ deep-scratch/steps/step21.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+class Function:
+    def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs] # variable 화
+
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs]) # 세대 설정
+            for output in outputs:
+                output.set_creator(self) # 연결 설정
+            self.inputs = inputs
+            # self.outputs = outputs
+            self.outputs = [weakref.ref(output) for output in outputs] # 약한참조로 해결
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self):
+        raise NotImplementedError
+    
+    def backward(self):
+        raise NotImplementedError
+
+
+class Variable:
+    def __init__(self, data, name=None): # name 지정
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} type is not available.')
+        self.data = data
+        self.name = name
+        self.creator = None
+        self.grad = None
+        self.generation = 0
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p +')'
+    
+    def __mul__(self, other):
+        return mul(self, other)
+    
+    def __add__(self, other):
+        return add(self, other)
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x:x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            # gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                # 조건문으로 grad 가 이미 있을 시엔 더해주도록 변경
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+                
+                if x.creator is not None:
+                    # funcs.append(x.creator)
+                    add_func(x.creator)
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # y 는 약한 참조(weakref)
+
+    def cleargrad(self):
+        self.grad = None
+    
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    f = Add()
+    return f(x0, x1)
+    
+if __name__ == '__main__':
+    x = Variable(np.array(2.0))
+    y = x + np.array(3.0)
+    print(y)
