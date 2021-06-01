a968b943b9df0001f8ec9059899d9aa7fa466a24
jayChung0302 <whtnek@gmail.com>
2021-06-02 03:26:15 +0900

    wip step33~34, add dezero.function

---
+++ deep-scratch/dezero/functions.py
@@ -0,0 +1,33 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero.core import Function
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x)
+        return gx
+
+def sin(x):
+    f = Sin()
+    return f(x)
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+def cos(x):
+    f = Cos()
+    return f(x)
