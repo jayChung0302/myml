4713383d712c7f130b6db763c58a9756881524d0
jayChung0302 <whtnek@gmail.com>
2021-06-02 17:26:17 +0900

    wip step35~37, add dezero function

--- deep-scratch/dezero/functions.py
+++ deep-scratch/dezero/functions.py
@@ -1,8 +1,10 @@
 if '__file__' in globals():
     import os, sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import numpy as np
 from dezero.core import Function
+from dezero import utils
 
 class Sin(Function):
     def forward(self, x):
@@ -31,3 +33,66 @@ class Cos(Function):
 def cos(x):
     f = Cos()
     return f(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y*y)
+        return gx
+
+def tanh(x):
+    f = Tanh()
+    return f(x)
+
+class Sum(Function):
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis,
+                                        self.keepdims)
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
+
+def reshape_sum_backward(gy, x_shape, axis, keepdims):
+    """Reshape gradient appropriately for dezero.functions.sum's backward.
+    Args:
+        gy (dezero.Variable): Gradient variable from the output by backprop.
+        x_shape (tuple): Shape used at sum function's forward.
+        axis (None or int or tuple of ints): Axis used at sum function's
+            forward.
+        keepdims (bool): Keepdims used at sum function's forward.
+    Returns:
+        dezero.Variable: Gradient variable which is reshaped appropriately
+    """
+    ndim = len(x_shape)
+    tupled_axis = axis
+    if axis is None:
+        tupled_axis = None
+    elif not isinstance(axis, tuple):
+        tupled_axis = (axis,)
+
+    if not (ndim == 0 or tupled_axis is None or keepdims):
+        actual_axis = [a if a >= 0 else a + ndim for a in tupled_axis]
+        shape = list(gy.shape)
+        for a in sorted(actual_axis):
+            shape.insert(a, 1)
+    else:
+        shape = gy.shape
+
+    gy = gy.reshape(shape)  # reshape
+    return gy
---
+++ deep-scratch/steps/step35.py
@@ -0,0 +1,26 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x = Variable(np.array(1.0))
+    y = F.tanh(x)
+    x.name = 'x'
+    y.name = 'y'
+    y.backward(create_graph=True)
+
+    iters = 3
+
+    
+    for i in range(iters):
+        gx = x.grad
+        x.cleargrad()
+        gx.backward(create_graph=True)
+    
+    gx = x.grad
+    gx.name = 'gx' + str(iters + 1)
