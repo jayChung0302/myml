da27db379c3b2eebdbf8100ba64e0da4b24d5b24
jayChung0302 <whtnek@gmail.com>
2021-07-14 18:50:51 +0900

    TCR wip

--- deep-scratch/memo.py
+++ deep-scratch/memo.py
@@ -1,5 +1,6 @@
 import sys, os
 import mypy
+from typing import List, Dict, Any
 from datetime import datetime
 import pickle
 
@@ -10,9 +11,13 @@ class Script:
         self.name = name
         self.date = str(datetime.today()).split()[0]
         self.time = str(datetime.today()).split()[1].split('.')[0]
-    
-    def add_hashtag(self, name):
-        self.hashtag.append(name)
+        self.content = self.content + ", " + self.date
+
+    def add_hashtag(self, name:Any):
+        if isinstance(name, str):
+            self.hashtag.append(name)
+        elif isinstance(name, List):
+            self.hashtag += name
     
     def del_hashtag(self, name):
         pass
@@ -23,7 +28,6 @@ class Memo:
         self.datelist = {}
         self.hashlist = {}
         self.name = "./auto_saved_memo"
-        # self.last_name = ""
         self.today = self.set_today()
 
         if memo_dir is not None:
@@ -55,24 +59,27 @@ class Memo:
             pickle.dump(self.hashlist, file)
 
     def put_script(self, script: Script):
-        if script.name in self.memo.keys():
-            print('Same script file name already exist.')
-            self.memo[script.name] = self.memo[script.name] + \
-                'Auto saved in {script.date}, {script.time}\n' + script.content
-            print('Auto saved')
-        else:
-            self.memo[script.name] = script.content
-
-        for hashname in script.hashtag:
-            if hashname in self.hashlist.keys():
-                self.hashlist[hashname].append(script.name)
+        if isinstance(script, Script):
+            if script.name in self.memo.keys():
+                print('Same script file name already exist.')
+                self.memo[script.name] = self.memo[script.name] + \
+                    f'\nAuto saved in {script.date}, {script.time} \n' + script.content + '\n'
+                print('Auto saved')
             else:
-                self.hashlist[hashname] = [script.name]
+                self.memo[script.name] = script.content + '\n'
 
-        if script.date not in self.datelist.keys():
-            self.datelist[script.date] = [script.name]
+            for hashname in script.hashtag:
+                if hashname in self.hashlist.keys():
+                    self.hashlist[hashname].append(script.name)
+                else:
+                    self.hashlist[hashname] = [script.name]
+
+            if script.date not in self.datelist.keys():
+                self.datelist[script.date] = [script.name]
+            else:
+                self.datelist[script.date].append(script.name)
         else:
-            self.datelist[script.date].append(script.name)
+            raise ValueError(f'{script} is not a Script instance')
 
     def show_memo(self):
         print(self.memo.keys())
@@ -80,18 +87,36 @@ class Memo:
     def load_script(self, load_dir="./"):
         pass
 
-    def see(self, script_name):
+    def read_script(self, script_name):
         print(self.memo[script_name])
 
 if __name__ == '__main__':
+    # try:
+    #     memo = Memo()
+    #     script = Script('0714_example')
+    #     script.content = '메모장을 만들어 봅시다 헤헤'
+    #     script.hashtag = ['ss', 'ww']
+    #     memo.put_script(script)
+    #     memo.total_save()
+    #     memo.show_memo()
+    #     memo.read_script('0714_example')
+    #     print(memo.datelist)
+    #     print(memo.hashlist)
     try:
+        script = Script('0714_hi')
+        script.content = 'hello world..!'
+        script.hashtag = ['ss', 'kk']
         memo = Memo()
-        script = Script('0714_example')
-        script.content = '메모장을 만들어 봅시다 헤헤'
-        script.hashtag = ['ss', 'ww']
+        memo.load_memo('./auto_saved_memo.pkl')
+
         memo.put_script(script)
         memo.total_save()
         memo.show_memo()
-        memo.see('0714_example')
+        print(memo.datelist)
+        print(memo.hashlist)
+        memo.read_script('0714_example')
+        print(memo.memo)
+        print(memo.read_script('0714_hi'))
+        
     except:
         raise KeyError
