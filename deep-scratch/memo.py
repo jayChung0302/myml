b173f468d7d10aca922d991d3e40dbd35e97b8bf
jayChung0302 <whtnek@gmail.com>
2021-07-14 15:31:17 +0900

    TCR wip

--- deep-scratch/memo.py
+++ deep-scratch/memo.py
@@ -1,14 +1,21 @@
 import sys, os
+import datetime
 import pickle
 
 class Memo:
     def __init__(self):
         self.memo = ""
-        self.name = "auto_saved_memo.txt"
+        self.datelist = []
+        self.name = "./auto_saved_memo.txt"
         self.last_name = ""
+        self.today = self.today()
     
-    def save(self, save_dir="./"):
+    def today(self):
         pass
+
+    def save(self, save_dir="./"):
+        with open(self.name + '.pkl', 'wb') as file:
+            pickle.dump(self.memo, file)
 
     def load(self, load_dir="./"):
         pass
