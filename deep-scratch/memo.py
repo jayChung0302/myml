0dec4cd780685acd79dfe7daeacb183d79734014
jayChung0302 <whtnek@gmail.com>
2021-07-14 15:48:57 +0900

    TCR wip

--- deep-scratch/memo.py
+++ deep-scratch/memo.py
@@ -1,17 +1,28 @@
 import sys, os
-import datetime
+from datetime import datetime
 import pickle
 
+class Script:
+    def __init__(self):
+        self.content = ""
+        self.hashtag = []
+    
+    def add_hashtag(self, name):
+        self.hashtag.append(name)
+    
+    def del_hashtag(self, name):
+        pass
+    
 class Memo:
     def __init__(self):
         self.memo = ""
         self.datelist = []
-        self.name = "./auto_saved_memo.txt"
+        self.name = "./auto_saved_memo"
         self.last_name = ""
-        self.today = self.today()
+        self.today = self.set_today()
     
-    def today(self):
-        pass
+    def set_today(self):
+        return str(datetime.today()).split()[0]
 
     def save(self, save_dir="./"):
         with open(self.name + '.pkl', 'wb') as file:
@@ -26,3 +37,4 @@ class Memo:
 if __name__ == '__main__':
     memo = Memo()
     memo.save()
+    print()
