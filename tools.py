86e52008207ad84751d842178bd89c0b89e6865c
jayChung0302 <whtnek@gmail.com>
2022-08-14 14:59:28 +0900

    Add torch.tensor to image function

--- tools.py
+++ tools.py
@@ -111,7 +111,16 @@ def mkdir(dir_path, exist_ok=False):
 
 
 def torch_to_image(inp, title=None):
-    pass
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
 
 
 def numpy_to_image():
