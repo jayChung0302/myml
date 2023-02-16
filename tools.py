0cdb87407e45af01256ea79e0f2f117f9e7ee40b
jayChung0302 <whtnek@gmail.com>
2023-02-16 16:20:16 +0900

    Add closure for measuring function's I/O time

--- tools.py
+++ tools.py
@@ -27,6 +27,22 @@ import time
 
 # __all__ = []
 
+import time
+
+def elapsed(f):
+    '''Measure elapsed time closure'''
+    def wrap(*args):
+        start_r = time.perf_counter()
+        start_p = time.process_time()
+        ret = f(*args)
+        end_r = time.perf_counter()
+        end_p = time.process_time()
+        elapsed_r = end_r - start_r
+        elapsed_p = end_p - start_p
+        print(f'{f.__name__} elapsed: {elapsed_r:.2f}sec (real) / {elapsed_p:.2f}sec (cpu)')
+
+        return ret
+    return wrap
 
 def get_today() -> List:
     '''Get today's year, month, day information list'''
